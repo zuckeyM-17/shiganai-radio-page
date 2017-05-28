d37a703b445fdb2fdecd5bc3738a935f927eaab7
jumtech <despairing.clock+jumGithub@gmail.com>
2017-05-28 13:16:36 +0900

    footterを整える

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -10,6 +10,8 @@ export class FooterComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    // TwitterボタンをActivate
+    !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location.toString())?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
   }
 
 }
