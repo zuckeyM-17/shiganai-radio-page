054623c8a57f90d4905113c6c7d373f4f55c5519
jumtech <despairing.clock+jumGithub@gmail.com>
2017-05-28 19:28:01 +0900

    フォームの入力後にお礼を表示する.

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -9,13 +9,13 @@ import { Http, Response } from '@angular/http';
   styleUrls: ['./form.component.css']
 })
 export class FormComponent implements OnInit {
+  isFormSended: boolean = false;
   feedback: Feedback = {
     id: null,
     tw_account: "",
     content: "",
     date: null,
   }
-  status: number;
 
   constructor(private http: Http) {
   }
@@ -24,15 +24,20 @@ export class FormComponent implements OnInit {
   }
 
   submitFeedback() {
+    if (!this.feedback.content) return;
     this.feedback.id = Number(new Date());
-    this.feedback.tw_account = this.feedback.tw_account;
-    this.feedback.content = this.feedback.content;
     this.feedback.date = new Date();
+    const that = this;
     this.sendFeedback().then(function(status){
+      if(status === 200) {
+        that.clearFeedback();
+        that.isFormSended = true;
+        setTimeout(() => {that.isFormSended = false}, 3000);
+      }
     });
   }
 
-  sendFeedback():Promise<number> {
+  sendFeedback(): Promise<number> {
     return axios.post(
       'https://hooks.slack.com/services/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'
       ,JSON.stringify({
@@ -47,7 +52,15 @@ export class FormComponent implements OnInit {
     })
     .catch(err => alert(err));
   }
-  makeSlackText():string {
+
+  clearFeedback() {
+    this.feedback.id = null;
+    this.feedback.tw_account = "";
+    this.feedback.content = "";
+    this.feedback.date = null;
+  }
+
+  makeSlackText(): string {
     const id = this.feedback.id;
     const tw_account = this.feedback.tw_account;
     const content = this.feedback.content;
@@ -59,8 +72,8 @@ export class FormComponent implements OnInit {
     result += date.getFullYear() + "/";
     result += (date.getMonth()+1) + "/";
     result += date.getDate() + " ";
-    result += date.getHours() + ":" + date.getMinutes() + "\n";
-    result += "<https://twitter.com/" + tw_account + "|" + tw_account + ">";
+    result += date.getHours() + ":" + ("0" + date.getMinutes()).slice(-2) + "\n";
+    result += tw_account ? "<https://twitter.com/" + tw_account + "|" + tw_account + ">" : "名無し";
     result += "さんからのメッセージです。\n";
     result += content;
     return result;
