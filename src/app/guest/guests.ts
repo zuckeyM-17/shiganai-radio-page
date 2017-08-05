025ca15487005e014a0433fb2c3d27fd071ba742
jumtech <despairing.clock+jumGithub@gmail.com>
2017-08-05 12:57:00 +0900

    set guests from source file

---
+++ src/app/guest/guests.ts
@@ -0,0 +1,15 @@
+export const GUESTS = [
+  {
+    "name": "murasaki",
+    "url": "",
+    "img": "../../assets/image/default.png",
+  }, {
+    "name": "mittuntun",
+    "url": "https://twitter.com/mittuntun?lang=ja",
+    "img": "../../assets/image/mittuntun.png",
+  },{
+    "name": "HKDnet",
+    "url": "https://twitter.com/HKDnet?lang=ja",
+    "img": "../../assets/image/HKDnet.jpg",
+  }
+];
