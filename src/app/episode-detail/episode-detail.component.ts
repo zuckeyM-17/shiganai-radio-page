d3453372e5f0f3483cb2b24113b57103ede2f2c7
kazuki matsumura <matsumura@fastmedia.jp>
2017-08-28 01:23:14 +0900

    use twitter official button

--- src/app/episode-detail/episode-detail.component.ts
+++ src/app/episode-detail/episode-detail.component.ts
@@ -33,6 +33,19 @@ export class EpisodeDetailComponent implements OnInit {
       const id = params['id'];
       this.getEpisode(id);
     });
+
+    // tweetボタンをactiveに
+    const result = !function(d, s, id) {
+      let js;
+      const fjs = d.getElementsByTagName(s)[0],
+      p = /^http:/.test(d.location.toString()) ? 'http' : 'https';
+      if (!d.getElementById(id)) {
+        js = d.createElement(s);
+        js.id = id;
+        js.src = p + '://platform.twitter.com/widgets.js';
+        fjs.parentNode.insertBefore(js, fjs);
+      }
+    } (document, 'script', 'twitter-wjs');
   }
 
   getEpisode(id: string): void {
